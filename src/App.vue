704ff2f92f04db707b2288e2b19dfeb13bdbe585
jiang1991 <jiang12196@126.com>
2017-08-09 23:25:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!--<router-view></router-view>-->
+    <helloWorld></helloWorld>
+  </div>
+</template>
+
+<script>
+import helloWorld from './components/helloWorld'
+export default {
+  components: {helloWorld}
+}
+
+</script>
+
---
+++ src/components/helloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container">
+<h1>Hello World</h1>
+<p>{{test}}</p>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'Hello World',
+    data () {
+      return {
+        test: '这是一个大的测试'
+      }
+    }
+  }
+</script>
+
+<style>
+.container {
+    color: red;
+}
+</style>
