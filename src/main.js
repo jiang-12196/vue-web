593afbcda5b1dbc747f8b176c98cc0130b709b20
jiang1991 <jiang12196@126.com>
2017-12-05 22:00:18 +0800

    can import vue lib

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import vueTable from 'vue-table2'
+
 import App from './App'
 import router from './router'
 import Corner from './components/Corner'
@@ -8,6 +10,7 @@ import Corner from './components/Corner'
 Vue.config.productionTip = false
 
 Vue.component('Corner', Corner)
+Vue.component('vueTable', vueTable)
 
 /* eslint-disable no-new */
 new Vue({
